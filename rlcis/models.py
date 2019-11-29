73bbb97df8dfe9edda624866fa37a58145a6b1bb
alexandroidii <Riccio.peacenlove@gmail.com>
2019-11-28 21:35:44 -0500

    Initial Database setup and first Incident model.
    Initial setup of Database and first models for Incident.

--- rlcis/models.py
+++ rlcis/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Incident(models.Model):
+	country = models.CharField(max_length=60)
+	region = models.CharField(max_length=60)
+	bribed_by = models.CharField(max_length=100)
+	bribed_by_other = models.CharField(max_length=100)
+	bribe_type = models.CharField(max_length=60)
+	bribe_type_other = models.CharField(max_length=60)
+	location = models.CharField(max_length=60)
+	first_occurence = models.DateTimeField('first occurence')
+	resolution_date = models.DateTimeField('resolution date')
+	reviewer = models.Foreignkey(Reviewer, on_delete=models.CASCADE)
+	
+class Reviewer(models.Model):
+	first_name = models.CharField(max_length=60)
+	last_name = models.CharField(max_length=60)
+	employee_id = models.IntegerField()
