82f2f75f1b12035d2b5e8d6dd7eaa906e704499d
langenet <robert@lange.ca>
2019-12-15 20:04:08 -0500

    modified admin.py

--- rlcis/admin.py
+++ rlcis/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import Incident, Reviewer
 
 # Register your models here.
+admin.site.site_header = "RLCIS Admin"
+admin.site.site_title = "RLCIS Admin Area"
+admin.site.index_title = "Welcome to the RLCIS Admin area"
+
 admin.site.register(Incident)
 admin.site.register(Reviewer)
