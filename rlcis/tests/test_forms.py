4eeaa87e8a5f29b6e79fd165dbc944bf4b820e90
alexandroidii <Riccio.peacenlove@gmail.com>
2019-12-18 08:55:37 -0500

    form test working now
    There was a test.py file in the root folder.
    
    I also added the *.pyc files to the .gitignore file as those keep poping up but are just the compiled code that we don't need to upload to git.

--- rlcis/tests/test_forms.py
+++ rlcis/tests/test_forms.py
@@ -2,25 +2,10 @@ from django.test import TestCase
 
 from rlcis.forms import IncidentForm
 
-class IncidentFormTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        #Set up non-modified objects used by all test methods
-        IncidentForm.objects.create(
-            incident_summary='Django unittest summary',
-            incident_details='Django unittest Details',
-            country='Canada',
-            region='Ontario',
-            location='Ottawa',
-            bribed_by='AG',
-            bribed_by_other='',
-            bribe_type='CA',
-            bribe_type_other='',
-            first_occurence='2019-12-17',
-            resolution_date='2019-12-18',
-            # reviewer',     Not sure if I need this or not
-        )
 
-        def test_country_field_equals(self):
-            form = IncidentForm()
-            self.assertEqual(form.fields['country'] == 'Canada')
+class IncidentFormTest(TestCase):
+    def test_incident_form_country_field_label(self):
+        form = IncidentForm()
+        self.assertTrue(
+            form.fields['country'].label == None
+            or form.fields['country'].label == 'Country')
