6c1dfc2adb65a9788b650413fd5cc88baa34d835
alexandroidii <Riccio.peacenlove@gmail.com>
2021-02-16 20:53:48 -0500

    still not working

--- rlcis/views.py
+++ rlcis/views.py
@@ -165,21 +165,21 @@ def incident_form(request, id=0):
             logger.debug(form.errors)
             logger.debug("form.is_valid() failed")
             # Need to return the cleaned data back to the form but it doesn't exist in the DB yet.
-            # incident = Incident.objects.get(pk=id)
-            # form = IncidentForm(instance=incident)
-            # files = IncidentDocument.objects.filter(incident=incident)
+            incident = Incident.objects.get(pk=id)
+            form = IncidentForm(instance=incident)
+            files = IncidentDocument.objects.filter(incident=incident)
 
             # The problem is this is triggered from an Ajax call so it goes into the 
             # success portion of the Ajax callback which calls the incidents. 
             #  need to find a way to reload the page with the incorrect data and validation
-            jsonData = json.dumps({
-                'filename': docToDel.filename(),
-                'id': docToDel.pk
-            })
+            # jsonData = json.dumps({
+            #     'filename': docToDel.filename(),
+            #     'id': docToDel.pk
+            # })
 
  
 
-            return HttpResponse(jsonData, content_type='json')
+            # return HttpResponse( content_type='json')
 
             context = {
                 'form': form,
