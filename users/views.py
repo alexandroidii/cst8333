a8e92c225240a26218316530ffbb6a449a2b44a9
langenet <robert@lange.ca>
2021-03-31 21:42:53 -0400

    Lower case email

--- users/views.py
+++ users/views.py
@@ -17,6 +17,7 @@ import threading
 from django.contrib.auth.forms import PasswordResetForm
 from django.db.models.query_utils import Q
 from django.views.generic.edit import FormView
+from .decorators import unauthenticated_user
 
 
 """
