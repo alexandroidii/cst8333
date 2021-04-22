d37dab51885c01ca77f1022cdcf411136c51aab8
alexandroidii <Riccio.peacenlove@gmail.com>
2021-04-22 08:16:53 -0400

    allowed_roles now loops through all groups

--- rlcs/decorator.py
+++ rlcs/decorator.py
@@ -23,9 +23,14 @@ def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
             group = None
+            allowed = False
             if request.user.groups.exists():
-                group = request.user.groups.all()[0].name
-            if group in allowed_roles or request.user.is_superuser:
+                groups = request.user.groups.all()
+            for group in groups:
+                if group.name in allowed_roles or request.user.is_superuser:
+                    allowed = True
+                    break
+            if allowed:
                 return view_func(request, *args, **kwargs)
             else:
                 return HttpResponse('You are not authorized to view this page')
