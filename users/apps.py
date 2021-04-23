e9a1a6f16d52c3cbfead61acec01e26b065129fd
alexandroidii <Riccio.peacenlove@gmail.com>
2021-04-23 12:55:46 -0400

    more clean up

--- users/apps.py
+++ users/apps.py
@@ -6,15 +6,5 @@ class UsersConfig(AppConfig):
     name = 'users'
 
     def ready(self):
-        # from django.contrib.sites.models import Site
-        # config = ConfigParser()
-        # config.read('cst8333/config.properties')
-        # print(config.sections())
-        # print(config['DEFAULT']['title'])
-        # print(config['domain']['host'])
-
-        # rlcs_site = Sites.objects.all()
-        # rlcs_site.domain = 'lange.ca'
-        # rlcs.save()
         import users.signals
 
